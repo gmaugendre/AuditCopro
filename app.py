11a920cdf7abeee89c912a6afc95cb6ca696af12
gmaugendre <gael_maugendre@hotmail.com>
2026-05-01 18:28:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import os
 from pathlib import Path
+import shutil
 
 # --- CONFIGURATION DE L'APPLI ---
 st.set_page_config(page_title="Audit Compta Automatisé", layout="wide")
