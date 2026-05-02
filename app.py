6ea6fd1c450a63b76c6686a5c68e64534218626e
gmaugendre <gael_maugendre@hotmail.com>
2026-05-02 22:14:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -489,7 +489,7 @@ with col1:
 
 # --- Saisie des 12 relevés de compte ---
 nb_fichiers = len(releves_files) if releves_files else 0
-if nb_fichiers > 0 and < 12:
+if 0 < nb_fichiers < 12:
     st.warning(f"⏳ Il manque {12 - nb_fichiers} relevé(s).")
 elif nb_fichiers > 12:
     st.error(f"🚫 Trop de fichiers : vous avez importé {nb_fichiers} relevés au lieu de 12.")
