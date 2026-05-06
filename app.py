29c8c2eba11fdec8735c8f510ec05ef2d6c5a70a
gmaugendre <gael_maugendre@hotmail.com>
2026-05-06 23:46:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -28,8 +28,8 @@ UPLOAD_DIR = "storage_compta"
 if not os.path.exists(UPLOAD_DIR):
     os.makedirs(UPLOAD_DIR)
 
-#GEMINI_MODEL="gemini-2.5-flash"
-GEMINI_MODEL="gemini-2.5-flash-lite"
+GEMINI_MODEL="gemini-2.5-flash"
+#GEMINI_MODEL="gemini-2.5-flash-lite"
 
 API_KEY = st.secrets["GEMINI_API_KEY4"]
 client = genai.Client(api_key=API_KEY, http_options={'api_version': 'v1beta'})
