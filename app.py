3efd2630afde4c10bf4c8ef0d5e93ca75fa0932d
gmaugendre <gael_maugendre@hotmail.com>
2026-05-06 14:59:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ if not os.path.exists(UPLOAD_DIR):
 GEMINI_MODEL="gemini-2.5-flash-lite"
 #GEMINI_MODEL="gemini-2.5-flash"
 
-API_KEY = st.secrets["GEMINI_API_KEY1"]
+API_KEY = st.secrets["GEMINI_API_KEY2"]
 client = genai.Client(api_key=API_KEY, http_options={'api_version': 'v1beta'})
 
 THRESHOLD_FUZZ=85
