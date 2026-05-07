0badffe3c47b2d5cc7f094a960ac641e56339a8d
gmaugendre <gael_maugendre@hotmail.com>
2026-05-07 17:31:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1285,7 +1285,7 @@ with col_logo:
 
     with open("Exemple_Rapport.pdf", "rb") as f:
         pdf_exemple = f.read()
-    st.download_button(label="📄 Exemple de rapport", data=pdf_exemple, file_name="Exemple_Rapport.pdf", mime="application/pdf", use_container_width=True)
+    st.download_button(label="📄 Exemple   de rapport", data=pdf_exemple, file_name="Exemple_Rapport.pdf", mime="application/pdf", use_container_width=True)
 
 st.markdown("---")
 
