4b74a10a5cb0a52ccfc498d820c0c35f56c2e521
gmaugendre <gael_maugendre@hotmail.com>
2026-05-07 16:48:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1257,9 +1257,9 @@ NOIR   = (30, 30, 30)
 col_texte, col_logo = st.columns([6, 1], vertical_alignment="center")
 
 with col_texte:
-    st.markdown(f'<h1 style="color: rgb{NAVY}; margin-bottom: 0;">Vérification des comptes de copropriété</h1>', unsafe_allow_html=True)
-    st.markdown(f'<h3 style="color: rgb{OR}; margin-top: 0;">Votre expert digital. Simple, automatique, immédiat et indépendant.</h3>', unsafe_allow_html=True)
-    st.markdown(f'<h3 style="color: rgb{OR};"><i>Conseils syndicaux : reprenez le contrôle !</i></h3>', unsafe_allow_html=True)
+    st.markdown(f'<h1 style="color: rgb{NAVY}; margin-bottom: 0;">Comptes de copropriété : reprenez le contrôle !</h1>', unsafe_allow_html=True)
+    st.markdown(f'<h3 style="color: rgb{OR}; margin-top: 0;">Votre expert digital indépendant : simple, automatique et immédiat.</h3>', unsafe_allow_html=True)
+    st.markdown(f'<h4 style="color: rgb{OR};"><i>La comptabilité de copropriété est complexe et ses documents souvent denses. Pourtant, chaque ligne du Grand Livre compte. Pour les conseils syndicaux, l'analyse manuelle de centaines de pages est une mission laborieuse. Nous mettons à votre disposition un assistant numérique performant pour simplifier votre analyse. Chargez vos documents en toute confidentialité et obtenez instantanément un rapport clair.</i></h4>', unsafe_allow_html=True)
 
 with col_logo:
     # On affiche le logo à droite
@@ -1583,9 +1583,7 @@ with col2:
         st.info("Charger tous les documents avant de lancer le traitement ...")
  
 st.markdown("---")
-st.markdown(" ###### Les comptes de copropriété sont souvent abscons pour les non-spécialistes, peuvent présenter des erreurs et manquer de transparence ; un grand livre peut comporter plus d'une centaine de pages d'écritures et les conseils syndicaux disposent de peu de moyens ou d'expertise pour assurer leur mission de contrôle des comptes.")
-st.markdown(" ###### Il s'agit d'un prototype mis à disposition gratuitement ; nous vous invitons à nous partager en retour votre expérience en tant qu'utilisateur (pertinence de l'analyse, besoins complémentaires etc.), par écrit (gael_maugendre@hotmail.com) ou de vive voix (+33 6 14 29 80 29)).")
-st.markdown("---")
+st.caption(" ###### Il s'agit d'un prototype mis à disposition gratuitement ; nous vous invitons à nous partager en retour votre expérience en tant qu'utilisateur (pertinence de l'analyse, besoins complémentaires etc.), par écrit (gael_maugendre@hotmail.com) ou de vive voix (+33 6 14 29 80 29)).")
 st.caption(" ###### Disclaimer : Cette application est un assistant digital conçu pour accompagner les Conseils syndicaux dans leur mission d'analyse et de contrôle des comptes de copropriété. Elle ne se substitue en aucun cas au pouvoir de contrôle des membres du Conseil syndical ni à l'expertise comptable du Syndic. Les éléments présentés dans le rapport d'analyse sont des pistes d'investigation qui peuvent comporter des erreurs de lecture automatisée, d'interprétation technique et doivent faire l'objet d'une vérification contradictoire, de contrôles sur pièces ainsi que de discussions avec le teneur de comptes.")
 st.caption(" ###### Protection des données : aucune donnée n'est conservée ; tous les fichiers restent confidentiels et sont intégralement supprimés dés la fin du traitement ; aucun rapport n'est enregistré.")
 st.caption(" ###### Tous droits réservés.")
