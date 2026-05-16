c92da46181633968d8128f20cb6fbc0d41f5f4fb
gmaugendre <gael_maugendre@hotmail.com>
2026-05-16 12:44:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ NB_RELEVES_BANCAIRES = 12    #METTRE 12 POUR UNE PERIODE ANNUELLE EN PRODUCTION!
 API_KEY = st.secrets[random.choice(["GEMINI_API_KEY1", "GEMINI_API_KEY2", "GEMINI_API_KEY3", "GEMINI_API_KEY4"])]    #Sélection aléatoire d'une clé à chaque exécution/chargement
 client = genai.Client(api_key=API_KEY, http_options={'api_version': 'v1beta'})
 
-THRESHOLD_FUZZ=85
+THRESHOLD_FUZZ = 85
 
 #POUR PATIENTER SI GEMINI EST EN PERIODE DE FORTE AFFLUENCE
 MAX_RETRIES = 3 #Nombre d'essais en tout pour chaque appel IA
